d353ac1b953df8f4bfa72ee8b23dac8794dce3cd
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-13 19:32:28 +0530

    Program to delete last element

---
+++ DataStructure.java
@@ -0,0 +1,102 @@
+package com.bridgelabz.linkedlist;
+
+/**
+ *Ability to delete the last element in the LinkedList of sequence 56->30->70
+ * Final Sequence: 56->30
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class DataStructure {
+	// Add element
+	public static void add() {
+	// create object
+		MyLinkedList list = new MyLinkedList();
+
+		list.add(70);
+		list.add(30);
+		list.add(56);
+
+		// print node
+		list.printNode();
+	}
+
+	// Append Method
+	public static void append() {
+		MyLinkedList list = new MyLinkedList();
+
+		// append node
+		list.append(56);
+		list.append(30);
+		list.append(70);
+
+		// print node
+		list.printNode();
+	}
+
+	// Insert node in LinkList
+	public static void insertNode() {
+		MyLinkedList list = new MyLinkedList();
+
+		// insert node
+		list.append(56);
+		list.append(70);
+		System.out.println("Before Inserting : ");
+		list.printNode();
+		list.insertNode(list.head, 30);
+		System.out.println("After Inserting : ");
+		// print node
+		list.printNode();
+	}
+
+	// Delete first element
+	public static void popFirst() {
+		MyLinkedList list = new MyLinkedList();
+		list.add(70);
+		list.add(30);
+		list.add(56);
+		list.printNode();
+
+		list.pop();
+		list.printNode();
+	}
+
+	// Delete last element
+	public static void popLast() {
+		MyLinkedList list = new MyLinkedList();
+		list.add(70);
+		list.add(30);
+		list.add(56);
+		System.out.println("\nBefore deleting last node : ");
+		list.printNode();
+
+		System.out.println("\nAfter deleting last node : ");
+		list.popLast();
+		list.printNode();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("-----------Welcome to linked list program---------");
+		System.out.println();
+
+		// Calling Add methods
+		System.out.println("Add Elements in List : ");
+		add();
+
+		// Calling Append Method
+		System.out.println("Appended element of Linked List :");
+		append();
+
+		// Calling Insert Method
+		System.out.println("Insert 30 in between 56 and 70");
+		insertNode();
+
+		// Calling popFirst method to delete first node
+		System.out.println("Delete first node : ");
+		popFirst();
+
+		// Calling popLast method to last first node
+		System.out.println("Delete last node : ");
+		popLast();
+	}
+}
